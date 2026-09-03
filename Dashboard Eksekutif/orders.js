ef047289f075cb48125ea375c8762a57cd97f7ed
supremiedrive-dot <supremiedrive@gmail.com>
2026-09-03 16:23:08 +0800

    Add files via upload

---
+++ Dashboard Eksekutif/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Pengecekan data spasial',
+        productNumber: 'Azka',
+        paymentStatus: '1',
+        status: 'Menunggu'
+    },
+    {
+        productName: 'Perbaikan data',
+        productNumber: 'Diffa',
+        paymentStatus: '10',
+        status: 'Ditolak'
+    },
+    {
+        productName: 'Validasi',
+        productNumber: 'Indriana',
+        paymentStatus: '1',
+        status: 'Selesai'
+    },
+]
